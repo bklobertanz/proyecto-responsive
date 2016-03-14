acedda4a8ff16575bebb5515925266d32c355281
Barís Klobertanz <klobertanz@gmail.com>
2016-03-14 00:52:54 -0300

    Versión final del proyecto en platzi

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var browserify=require("browserify");
 var uglify=require("gulp-uglify");
 var source=require("vinyl-source-stream");
 var buffer=require("vinyl-buffer");
+var smoosher = require('gulp-smoosher');
+var imageOP = require ('gulp-image-optimization');
 
 var config={
 
@@ -25,6 +27,10 @@ var config={
 		main:"./src/scripts/main.js",
 		watch:'./src/scripts/**/*.js',
 		output:'./build/js'
+	},
+	images:{
+		watch:['./build/img/*.jpg','./build/img/*.png'],
+		output: './dist/img'
 	}
 }
 /*implementando las tareas*/
@@ -56,12 +62,27 @@ gulp.task("build:css",function(){
 	.pipe(minifyCSS())
 	.pipe(gulp.dest(config.styles.output));
 });
+gulp.task('images', function(){
+	gulp.src(config.images.watch)
+	.pipe(imageOP({
+		optimizationLevel: 5,
+		progressive: true,
+		interlaced: true
+	}))
+	.pipe(gulp.dest(config.images.output))
+})
+gulp.task('inline',function(){
+	gulp.src('./build/index.html')
+	.pipe(smoosher())
+	.pipe(gulp.dest('./dist'))
+})
 gulp.task("watch",function(){
+	gulp.watch(config.images.watch,['images']);
   gulp.watch(config.scripts.watch,["build:js"]);
 	gulp.watch(config.styles.watch,["build:css"]);
 	gulp.watch(config.html.watch,["build"]);
 
 });
 
-gulp.task("build",["build:css",'build:js']);
+gulp.task("build",["build:css",'build:js','images','inline']);
 gulp.task("default",["server","watch","build"]);
